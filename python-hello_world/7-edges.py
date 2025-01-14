3e60b4b0cad557f8955dc9310c8c87db31f9096f
vidad <vidad31@gmail.com>
2025-01-14 18:42:38 +0100

    edges

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]  # prend les 3 premieres lettres
+word_last_2 = word[-2:]  # prend les 2 dernieres lettres
+middle_word = word[1:-1]  # exclut la 1er et la derniere lettre
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
