9b92efa30bbd0c273105e31dc826e8492434fc49
vidad <vidad31@gmail.com>
2025-01-14 20:03:51 +0100

    supp commentaire

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[:3]  # prend les 3 premieres lettres
-word_last_2 = word[-2:]  # prend les 2 dernieres lettres
-middle_word = word[1:-1]  # exclut la 1er et la derniere lettre
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
