6982b260a613dee8aca79d63742a90c9cd10e7b6
vidad <vidad31@gmail.com>
2025-01-21 11:48:56 +0100

    search and replace no comment

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-# Si num est égal à search, il est remplacé par replace.Sinon, num reste inchangé
     return [replace if num == search else num for num in my_list]
