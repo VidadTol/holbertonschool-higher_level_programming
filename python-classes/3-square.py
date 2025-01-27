dfa05a6fa9ab2a74f16901f74a70d61c1ce7751c
vidad <vidad31@gmail.com>
2025-01-27 14:52:03 +0100

    Doctring module and class

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
+"""
+    this module defines a class named Square with a private
+    instance attribute size
+    and a public instance method area that returns the area of the square
+    """
+
 
 class Square:
     """
-    This is a class named Square with a private instance attribute
-    """
+    a class that defines a square by the size of one
+    of its sides includes a method to compute
+    the area of the square.
+
+    Attributes:
+        size (int): The size of the square
+"""
 
     def __init__(self, size=0):
         """
-        The constructor of the Square class
+        initialize a new square instance
 
         Args:
-            size (int): The size of the square
+            size (int): The size of the square (default = 0)
+            must be an integer that is 0 or greater
 
         Raises:
             TypeError: If size is not an integer
@@ -23,4 +35,10 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """
+        calculates and returns the area of the square
+
+        Returns:
+            int: the area of the square
+        """
         return self.__size ** 2
