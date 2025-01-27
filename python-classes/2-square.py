eda0c136e9316b6809d4ccb23cc0140bf92e6d24
vidad <vidad31@gmail.com>
2025-01-27 12:53:18 +0100

    Doctring module and class

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    This module defines a class Square that represents a square.
+"""
+
     def __init__(self, size=0):
-        self.__size = size
+        """"
+        Initializes a new Square instance.
+
+        Parameters
+        ----------
+        size : int, optional
+            The size of one side of the square (default is 0).
+            Must be an integer and greater than or equal to 0.
+
+        Raises
+        ------
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
