45db7bc134e9bf1f8ade10119dbc009d6f09cde4
vidad <vidad31@gmail.com>
2025-01-27 14:30:13 +0100

    Doctring module and class

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    This is a class named Square with a private instance attribute
+    """
+
     def __init__(self, size=0):
+        """
+        The constructor of the Square class
+
+        Args:
+            size (int): The size of the square
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
