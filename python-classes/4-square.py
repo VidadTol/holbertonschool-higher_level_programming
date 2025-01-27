35e19542dda8e872c884cb573554fec50fc9f774
vidad <vidad31@gmail.com>
2025-01-27 17:03:19 +0100

    Doctring

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,57 +1,29 @@
 #!/usr/bin/python3
 """
-    This is a class named Square with a private instance attribute size
-    and a getter and setter for size attribute and a method that returns
+    This module defines a class Square
     """
 
 
 class Square:
     """
-    This is a class named Square with a private instance attribute size
-
-    Attributes:
-        size (int): The size of the square object
-
-    Methods:
-        __init__(self, size=0): The constructor of the Square class
-        area(self): Returns the area of the square object
+    The Square class includes a private instance attribute size with
+    property methods to retrieve and set it. The size must be an integer
+    (raises TypeError if not) and ≥ 0 (raises ValueError if not).
+    The class is instantiated with an optional size parameter
+    using __init__(self, size=0).
+    It includes a public instance method area that returns the current
+    square area.
     """
 
     def __init__(self, size=0):
-        """
-        The constructor of the Square class
-
-        Args:
-            size (int, optional): _description_. Defaults to 0.
-
-        Raises:
-            TypeError: If size is not an integer
-            ValueError: If size is less than 0
-        """
         self.__size = size
 
     @property
     def size(self):
-        """
-        getter for the size attribute of the square class
-
-        Returns:
-            int: the size of the square object
-        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        setter for the size attribute of the square class
-
-        Args:
-            value (int): the size of the square object
-
-        Raises:
-            TypeError: If size is not an integer
-            ValueError: If size is less than 0
-        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -59,10 +31,4 @@ class Square:
         self.__size = value
 
     def area(self):
-        """
-        calculates and returns the area of the square
-
-        Returns:
-            int: the area of the square
-        """
         return self.__size ** 2
