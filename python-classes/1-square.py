fbd4da2c978c8b2779c53d5c94d0e6f04fa3d574
vidad <vidad31@gmail.com>
2025-01-27 12:39:43 +0100

    Doctring module and class

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square that represents a square.
+"""
+
 
 class Square:
+    """
+    A class used to represent a Square.
+    ...
+    Attributes
+    ----------
+    __size : int 
+    The size of one side of the square
+    """
     def __init__(self, size):
+        """
+        Initializes the Square with the given size.
+        Parameters
+        ----------
+        size : int 
+        The size of one side of the square.
+        """
         self.__size = size
