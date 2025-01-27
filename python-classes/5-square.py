2b43e87602d60b418e05b51d5ee91eb5d1337b5d
vidad <vidad31@gmail.com>
2025-01-27 16:59:16 +0100

    Doctring

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,63 +1,32 @@
 #!/usr/bin/python3
 """
-    This is a class named Square with a private instance attribute size
-    and a getter and setter for size attribute
-    and a method that returns the area
-    of the square object and prints the square object using the character #
-    """
+    This module defines a class Square
+"""
 
 
 class Square:
 
     """
-    This is a class named Square with a private instance attribute size
-
-    Attributes:
-        size (int): The size of the square object
-
-    Methods:
-        __init__(self, size=0): The constructor of the Square class
-        area(self): Returns the area of the square object
-        my_print(self): Prints the square object
+    The Square class features a private instance attribute size with property
+    methods to retrieve and set it.
+    The size must be an integer (raises TypeError if not) and ≥ 0
+    (raises ValueError if not).
+    The class is instantiated with an optional size parameter
+    using __init__(self, size=0).
+    It includes a public instance method area that returns
+    the current square area,and a public instance method
+    my_print that prints the square with # in stdout.
+    If size is 0, an empty line is printed.
     """
     def __init__(self, size=0):
-        """
-        The constructor of the Square class
-
-        Args:
-            size (int, optional): _description_. Defaults to 0.
-            must be an integer
-            and greater than or equal to 0
-
-        Raises:
-            TypeError: If size is not an integer
-            ValueError: If size is less than 0
-        """
-
         self.size = size
 
     @property
     def size(self):
-        """
-        getter for the size attribute of the square class
-
-        Returns:
-            int: the size of the square object
-        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        setter for the size attribute of the square class
-
-        Args:
-            value (int): the size of the square object
-
-        Raises:
-            TypeError: If size is not an integer
-            ValueError: If size is less than 0
-        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -65,21 +34,9 @@ class Square:
         self.__size = value
 
     def area(self):
-        """
-        Returns the area of the square object
-
-        Returns:
-            int: the area of the square object
-        """
         return self.__size ** 2
 
     def my_print(self):
-        """
-        Prints the square using the character #
-        or a new line if size is 0 print
-        an empty line
-        """
-
         if self.__size == 0:
             print()
         for x in range(self.__size):
