06471edf63615d47883c3e27734c940820a8612c
vidad <vidad31@gmail.com>
2025-01-27 14:39:54 +0100

    Doctring module and class

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square with a private instance attribute 'size',
+and includes methods for initialization and validating the size.
+"""
+
 
 class Square:
     """
     This is a class named Square with a private instance attribute
+
+    attributes:
+        size (int): The size of the square
     """
 
     def __init__(self, size=0):
         """
-        The constructor of the Square class
+        initializes the square with a size
 
         Args:
-            size (int): The size of the square
+            size (int): The size of the square (default is 0)
+
+        must be an integer that is 0 or greater
 
         Raises:
             TypeError: If size is not an integer
