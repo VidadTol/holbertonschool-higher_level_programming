26170d968c0d6d967e55d3b47bca8f8cc756e34b
vidad <vidad31@gmail.com>
2025-02-03 13:31:52 +0100

    Checks if obj is an instance of a class that inherited from a_class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Contains a function that checks if an object is an instance
+of a class that inherited from the specified class.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+Checks if obj is an instance of a class that inherited from a_class
+"""
+
+    return isinstance(obj, a_class) and type(obj) is not a_class
