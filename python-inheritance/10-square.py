a0fe5f7b38726966f2cae905bc93144713ae9bcb
vidad <vidad31@gmail.com>
2025-02-03 16:13:43 +0100

    pycodestyle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -38,6 +38,7 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"
 
+
 class Square(Rectangle):
     """
     A class Square that inherits from Rectangle
