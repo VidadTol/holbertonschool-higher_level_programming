53d82976160eebd6013a8268b301fcc19128ad4d
vidad <vidad31@gmail.com>
2025-02-03 11:59:23 +0100

    class MyList that inherits from list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """
+Write a class MyList that inherits from list
     """
 
-class My_list:
-    def __init__(self, list):
-        self.list = list
-    
+
+class MyList(list):
+    """
+    The class MyList, which inherits from the list, includes a method
+    that prints the list sorted in ascending order.
+    """
+
     def print_sorted(self):
-        print(sorted(self.list))
+        print(sorted(self))
