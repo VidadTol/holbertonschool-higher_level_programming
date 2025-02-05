5a30deeb5566e47cd84d8dcbd9eb40510e45095d
vidad <vidad31@gmail.com>
2025-02-05 10:54:57 +0100

    supp commentaire trop

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -10,9 +10,6 @@ class VerboseList(list):
     def append(self, item):
         """
         Append an item to the list and print a message.
-
-        Args:
-            item: The item to append to the list.
         """
 
         super().append(item)
@@ -22,9 +19,6 @@ class VerboseList(list):
         """
         Extend the list by appending elements from the iterable
         and print a message.
-
-        Args:
-            item: An iterable with elements to add to the list.
         """
         super().extend(item)
         print(f"Extended the list with [{len(item)}] items.")
@@ -33,9 +27,6 @@ class VerboseList(list):
         """
         Remove the first occurrence of an item from the list
         and print a message.
-
-        Args:
-            item: The item to remove from the list.
         """
         if item not in self:
             print(f"Item [{item}] not found in the list.")
@@ -48,17 +39,6 @@ class VerboseList(list):
         Remove and return the item at the given position in the
         list and print a message. If no index is specified,
         removes and returns the last item in the list.
-
-        Args:
-            index: The position of the item to remove.
-            Defaults to -1 (last item).
-
-        Returns:
-            The item removed from the list.
-
-        Raises:
-            IndexError: If the list is empty or the index
-            is out of range.
         """
         try:
             item = super().pop(index)
