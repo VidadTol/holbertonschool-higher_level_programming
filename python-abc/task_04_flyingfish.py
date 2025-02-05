8d59bc3ed37281c56651c445a656eca7b726498d
vidad <vidad31@gmail.com>
2025-02-05 12:16:08 +0100

    flyingfish

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+
+    """
+
+
+class Fish:
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitat(self):
+        print("The fish lives in water")
+
+
+class Bird:
+    def fly(self):
+        print("The bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    def __init__(self):
+        pass
+        print(FlyingFish.mro())
+
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
