8f72b412f6598aad9157301c7ecb4b63a4da2069
vidad <vidad31@gmail.com>
2025-02-06 11:25:21 +0100

    modif import sans py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,7 +2,7 @@
 """
 This module definies a class BaseGeometry
 """
-Rectangle = __import__("9-rectangle.py").Rectangle
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
