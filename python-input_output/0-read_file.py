0e3f5e2195e7f86ad9693417f6dbaa9596bc5261
vidad <vidad31@gmail.com>
2025-02-10 11:30:27 +0100

    modif code

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+"""
+This module defines a function read_file that
+reads a text file and prints its content.
+"""
+
+
 def read_file(filename=""):
-   with open('my_file_0.txt', 'r', encoding='utf-8') as file:
-    content = file.read()
-    print(content)
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+        print(content)
