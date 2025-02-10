bbf64449bb5660fd6d94d0bb8c18690aee8a2d95
vidad <vidad31@gmail.com>
2025-02-10 12:18:54 +0100

    write file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines a function write_file
+that writes a string to a text file.
+"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
