2f8f1157480cd742d6bd630591ba080e3f426aa4
vidad <vidad31@gmail.com>
2025-02-11 15:25:22 +0100

    ajout du module Student avec une méthode to_json pour récupérer une représentation dictionnaire d'une instance

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module defines a class Student with a method to_json
+that retrieves a dictionary representation of a Student instance
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            my_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    my_dict[key] = self.__dict__[key]
+            return my_dict
