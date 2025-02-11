048163f7761a3024bebf3de586536ef0c9b53a1e
vidad <vidad31@gmail.com>
2025-02-11 13:26:01 +0100

    simplification de la logique de chargement de la liste dans le script 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,13 +12,9 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 filename = "add_item.json"
 
 if os.path.exists(filename):
-    if os.path.getsize(filename) == 0:
-        my_list = []
-    else:
-        my_list = load_from_json_file(filename)
+    my_list = load_from_json_file(filename)
 else:
     my_list = []
 
 my_list.extend(sys.argv[1:])
-
 save_to_json_file(my_list, filename)
