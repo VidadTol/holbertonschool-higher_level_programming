a403b87963a4165e6c7c0ff6622a042064463015
vidad <vidad31@gmail.com>
2025-02-11 17:55:21 +0100

    doc: improve docstring formatting in append_write function for better readability

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -7,11 +7,14 @@ that appends a string to a text file.
 
 def append_write(filename="", text=""):
     """
-    Append a string to a text file (UTF8) and return the number of characters added.
+    Append a string to a text file (UTF8) and return
+    the number of characters added.
 
     Args:
-        filename (str): The name of the file to append to. Defaults to an empty string.
-        text (str): The text to append to the file. Defaults to an empty string.
+        filename (str): The name of the file to append to.
+        Defaults to an empty string.
+        text (str): The text to append to the file.
+        Defaults to an empty string.
 
     Returns:
         int: The number of characters added.
