5313e9108c8840745e4cf2614dd3abdf1180c833
vidad <vidad31@gmail.com>
2025-02-18 15:37:59 +0100

    feat: add a simple Flask API with user management endpoints

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+"""
+Simple Flask API
+
+This script creates a basic Flask API server.
+It handles GET and POST requests and responds with plain text or JSON data
+depending on the endpoint.
+"""
+
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+
+app = Flask(__name__)
+
+users = {}
+
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API"
+
+
+@app.route("/data")
+def get_username():
+    return jsonify(list(users.keys()))
+
+
+@app.route("/status")
+def status():
+    return "ok"
+
+
+@app.route("/users/<username>")
+def get_user(username):
+    user_info = users.get(username)
+    if user_info:
+        return jsonify(user_info)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    user_data = request.get_json()
+    username = user_data.get("username")
+
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+    else:
+        users[username] = user_data
+        return jsonify({"message": "User added", "user": user_data}), 201
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
