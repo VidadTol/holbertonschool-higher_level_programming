8a8cb2e757cbcf0e1f47c6f0980876cdf39d3d51
vidad <vidad31@gmail.com>
2025-02-19 16:51:34 +0100

    refactor: update route docstrings for clarity and consistency

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,54 +1,42 @@
 #!/usr/bin/python3
-
-"""
-Simple Flask API
-
-This script creates a basic Flask API server.
-It handles GET and POST requests and responds with plain text or JSON data
-depending on the endpoint.
-"""
-
-
-from flask import Flask
-from flask import jsonify
-from flask import request
+"""Module for handling routes with Flask to respond to different endpoints"""
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
 users = {}
 
 
-@app.route("/")
+@app.route('/')
 def home():
-    """ respond with a welcome message """
-    return "Welcome to the Flask API"
+    """Return a str in root path"""
+    return "Welcome to the Flask API!"
 
 
-@app.route("/data")
+@app.route('/data')
 def list_users():
-    """ respond with a username """
+    """Return list of users in data path"""
     return jsonify(list(users.keys()))
 
 
-@app.route("/status")
+@app.route('/status')
 def send_status():
-    """ respond with a status message """
+    """Return a str in status path"""
     return "OK"
 
 
-@app.route("/users/<username>")
+@app.route('/users/<username>')
 def get_user(username):
-    """ respond with user data """
+    """Return a user data in users dynamic path"""
     if username in users:
         return jsonify(users[username])
     return jsonify({"error": "User not found"}), 404
 
 
-@app.route("/add_user", methods=["POST"])
+@app.route('/add_user', methods=['POST'])
 def add_user():
-    """ add a new user """
+    """Return a user in users dict"""
     data = request.get_json()
-
     if not data or "username" not in data:
         return jsonify({"error": "Username is required"}), 400
     users[data["username"]] = data
