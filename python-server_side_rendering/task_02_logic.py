692bb3fe7efe9620be8f7457a238de132a530619
vidad <vidad31@gmail.com>
2025-03-21 16:46:35 +0100

    Rename variable 'items' to 'list' in items function for clarity

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -19,8 +19,8 @@ def contact():
 def items():
     with open('items.json', 'r') as file:
         data = json.load(file)
-        items = data.get('items', [])
-    return render_template('items.html', items=items)
+        list = data.get('items', [])
+    return render_template('items.html', list=list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
