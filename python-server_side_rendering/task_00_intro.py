3db1551371451d61f9eef8dcfe9014aec522157e
vidad <vidad31@gmail.com>
2025-03-21 11:17:56 +0100

    Creating a Simple Templating Program

---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+import os
+
+
+def generate_invitations(template, attendees):
+    """ Check Imput Types"""
+    try:
+        if not isinstance(template, str):
+            raise TypeError('Error: template must be a string')
+        if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
+            raise TypeError('Error: attendees must be a list of dictionnaries')
+    
+    except TypeError as e:
+        print("TypeError: {}".format(e))
+        return
+    
+    """ Handle Empty List"""
+    try:
+        if not template:
+            raise ValueError("Error: template is empty")
+      
+        if not attendees:
+            raise ValueError("Error: the participant list is empty")
+    
+    except ValueError as e:
+        print("ValueError: {}".format(e))
+        return
+    
+    """ treatment of each paticipant"""
+    try:
+        os.makedirs("output", exist_ok=True)
+        for i, attendee in enumerate(attendees):
+            invitation = template
+
+            """ Replacement of reserved spaces """
+            if "name" in attendee:
+                invitation = invitation.replace("{name}", attendee["name"])
+            else:
+                invitation = invitation.replace("{name}", "N/A")
+
+            if "event_title" in attendee:
+                invitation = invitation.replace("{event_title}", attendee["event_title"])
+            else:
+                invitation = invitation.replace("{event_title}", "N/A")
+
+            if "event_date" in attendee:
+                invitation = invitation.replace("{event_date}", str(attendee["event_date"])) 
+            else:
+                invitation = invitation.replace("{event_date}", "N/A")
+
+            if "event_location" in attendee:
+                invitation = invitation.replace("{event_location}", attendee["event_location"])
+            else:
+                invitation = invitation.replace("{event_location}", "N/A")
+
+            """Creating output files """
+            filename = f"output/invitation_{i + 1}.txt"
+            if os.path.exists(filename):
+                print("file '{}' already exists".format(filename))
+            try:
+                with open(filename, "w") as file:
+                    file.write(invitation)
+            except OSError as e:
+                print(f"Error writing file {filename}: {e}")
+                return
+    except OSError as e:
+        print(f"OSError creating output directory: {e}")
+        return
