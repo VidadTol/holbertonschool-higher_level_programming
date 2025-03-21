42c75defbdcbc47962d18a4f34829a13ae08cb6a
vidad <vidad31@gmail.com>
2025-03-21 16:59:17 +0100

    modif

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Flask, render_template, request
 import json
 import csv
@@ -7,8 +8,8 @@ app = Flask(__name__)
 def json_file(filepath):
     with open(filepath, 'r') as file:
         return json.load(file)
-
-def read_csv_data(filepath):
+    
+def csv_file(filepath):
     products = []
     with open(filepath, 'r') as file:
         reader = csv.DictReader(file)
@@ -33,28 +34,28 @@ def contact():
 @app.route('/items')
 def items():
     with open('items.json', 'r') as file:
-        data = json.load(file)
-        list = data.get('items', [])
+        data  = json.load(file)
+    list = data.get('items', [])
     return render_template('items.html', list=list)
 
 @app.route('/products')
 def products():
     source = request.args.get('source')
     id = request.args.get('id', type=int)
-
+    
     if source == 'json':
         products = json_file('products.json')
     elif source == 'csv':
-        products = read_csv_data('products.csv')
+        products = csv_file('products.csv')
     else:
-        return render_template('products_display.html', message="Wrong source")
-
+        return render_template('product_display.html', message="Wrong source")
+    
     if id:
-        products = [products for product in products if product.get('id') == id]
+        products = [product for product in products if product['id'] == id]
         if not products:
-            return render_template('products_display.html', message="Product not found")
-
-    return render_template('products_display.html', products=products)
+            return render_template('product_display.html', message="Product not found")
+    
+    return render_template('product_display.html', products=products)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
